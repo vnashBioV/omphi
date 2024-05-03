b316b36e427c99063d5ae3dfc109b3da04b56f67
tshilidzi <tshilidzirambuda9@gmail.com>
2024-05-03 15:53:45 +0200

    first commit

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -4,16 +4,7 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import { FaArrowRight } from "react-icons/fa6";
 
-interface CustomButtonProps {
-  path: string;
-  customeStyles: string;
-  color: string;
-  text: string;
-  buttonParent: string;
-  iconStyles: string;
-}
-
-const CustomButton: React.FC<CustomButtonProps> = ({ path, customeStyles, color, text, buttonParent, iconStyles}) => {
+const CustomButton = ({ path, customeStyles, color, text, buttonParent, iconStyles}) => {
   return (
     <div className={`${buttonParent}`}>
       <Link 
