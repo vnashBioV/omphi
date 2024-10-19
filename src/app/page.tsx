e80036ec55319d05754d3bb4f24669346ac07c82
vnashBioV <tshilidzirambuda9@gmail.com>
2024-10-20 00:45:44 +0200

    adding meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Posts from "@/components/Posts";
 import Contact from "@/components/Contact";
 import BackgroundArt from "@/components/BackgroundArt";
 import Showcase from '@/components/Showcase';
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "tshilitech",
@@ -16,6 +17,9 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className="flex relative min-h-[100vh] flex-col items-center justify-between">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-8346213375529687"/>
+      </Head>
       <BackgroundArt/>
       <Banner/>
       <About/>
